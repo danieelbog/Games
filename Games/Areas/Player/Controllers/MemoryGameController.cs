b98da396e960cb88e65a34291e3e0359c01ed8aa
Daniel Bogdan <danieel.bogdan@gmail.com>
2020-05-28 02:26:25 +0300

    Add project files.

---
+++ Games/Areas/Player/Controllers/MemoryGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Games.Areas.Player.Controllers
+{
+    public class MemoryGameController : Controller
+    {
+        [Area("Player")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
