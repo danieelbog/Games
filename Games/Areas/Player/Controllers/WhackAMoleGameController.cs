a66d42c455738226051a207e566c47dfc6359dfc
Daniel Bogdan <danieel.bogdan@gmail.com>
2020-05-29 04:18:14 +0300

    wack a mole game

---
+++ Games/Areas/Player/Controllers/WhackAMoleGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Games.Areas.Player.Controllers
+{
+    public class WhackAMoleGameController : Controller
+    {
+        [Area("Player")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
